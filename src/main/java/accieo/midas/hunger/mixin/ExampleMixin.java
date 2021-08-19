e231416b965b1e5265b1a17e3b69e24defcb677f
Accieo <42718776+Accieo@users.noreply.github.com>
2021-08-19 20:55:18 +0200

    Removed ExampleMixin.java

--- src/main/java/accieo/midas/hunger/mixin/ExampleMixin.java
+++
@@ -1,9 +0,0 @@
-package accieo.midas.hunger.mixin;
-
-import net.minecraft.client.gui.screen.TitleScreen;
-import org.spongepowered.asm.mixin.Mixin;
-
-@Mixin(TitleScreen.class)
-public class ExampleMixin {
-	
-}
